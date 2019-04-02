a887aaf9a93ce32e02b0ea877b0de3b27d0a1d91
20190109 <will24cm@login01.cluster.cs.jmu.edu>
2019-04-02 15:22:31 -0400

    Parallel version of threefry

---
+++ ThreeFry/threefry_par.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include "threefry.h"
+
+#ifdef _OPENMP
+# include <omp.h>
+# define START_TIMER(X) double _timer_ ## X = omp_get_wtime();
+# define STOP_TIMER(X) _timer_ ## X = omp_get_wtime() - (_timer_ ## X);
+# define GET_TIMER(X) (_timer_ ## X)
+#else
+# include <sys/time.h>
+  struct timeval _tv;
+#define START_TIMER(X) gettimeofday(&_tv, NULL); \
+    double _timer_ ## X = _tv.tv_sec+(_tv.tv_usec/1000000.0);
+#define STOP_TIMER(X) gettimeofday(&_tv, NULL); \
+    _timer_ ## X = _tv.tv_sec+(_tv.tv_usec/1000000.0) - (_timer_ ## X);
+#define GET_TIMER(X) (_timer_ ## X)
+#endif
+
+//Uncomment this line for raw output
+//#define DEBUG
+
+/**
+ *  Parallel version of the Threefry Method
+ *
+ *  How it works
+ *
+ *  Threefry works by iterating over its key and counter values over a series of rounds. 
+ *   Every round, a mix function is executed over the data to produce the next value of the data. Every four rounds,
+ *   the initial key is added to the current value. After the last round, a final addition of the key 
+ *   is applied, and the full data is xored with the initial value. The result of this process is one 
+ *   randomly generated number in a sequence. 
+ *
+ *  Threefry works well in parallel systems because it is partially based off of a linearly-incrementing counter. 
+ *  By increasing this counter by a set amount, you can effectively and easily divide out the work of generating
+ *  random numbers amongst many nodes, using a single seed.
+ *  
+ *  Weaknesses of this Method
+ *
+ *
+ */
+int main(int argc, char* argv[]) {
+    long seed;
+    long count;    
+
+    threefry2x64_ctr_t* results;
+
+    if(argc < 3){
+        /**
+          *  Usage: ./threefry_ser unsigned long seed, unsigned long count
+          */
+        printf("USAGE: 3fry_ser seed(unsigned long) count(unsigned long)\n");
+        exit(EXIT_FAILURE);
+    } 
+
+    seed = atol(argv[1]);
+    count = atol(argv[2]);
+
+    results = malloc(sizeof(threefry2x64_ctr_t) * count);
+
+    START_TIMER(genvals);
+    #ifdef _OPENMP
+    #pragma omp parallel
+    #endif
+    {
+        int start = ( (count / omp_get_max_threads() ) * omp_get_thread_num());
+        #ifdef DEBUG
+        printf("Value of start is %i\n", start);
+        #endif
+    	threefry2x64_key_t key = {{seed,0}};
+    	threefry2x64_ctr_t ctr = {{start ,0}};
+
+    	for(int i = 0; i < (count/omp_get_max_threads()); i++){
+        	results[start + i] = threefry2x64(ctr, key);
+        	ctr.v[0]++;
+                
+    	}
+    }
+
+    STOP_TIMER(genvals);
+
+    #ifdef DEBUG
+    printf("Final Results: [");
+    for(int j = 0; j < count; j++){
+        printf(" %lu ",results[j]); 
+    }
+    printf("]\n");
+    #endif
+
+    printf("Time taken: %.6f\n", GET_TIMER(genvals));
+
+    free(results);
+
+    return 0;
+
+}
