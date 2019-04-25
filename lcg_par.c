c6ddcca04f336cbcee2bf769c3f09f7a9b935cfb
20190109 <meder2mm@login01.cluster.cs.jmu.edu>
2019-04-25 17:18:54 -0400

    fixed parallel generation

--- lcg_par.c
+++ lcg_par.c
@@ -32,48 +32,45 @@ inline int rand() {
 }
  
 int main(int argc, char* argv[]) {
-    int i;
 //    printf("rand max is %d\n", RAND_MAX);
 
     #ifndef CATOUT
     FILE* output = fopen("out_lcg_par.txt", "w");
     #endif
-
-    int myRank = 0;
     rseed = atoi(argv[1]);
     int size = atoi(argv[2]);
     int vals[size+200];
-    int mySize;
             
     #ifdef _OPENMP
     #pragma omp parallel 
     {   
-    myRank =  omp_get_thread_num();
-    mySize = size/omp_get_max_threads();
-    }
-    #endif
+      int myRank;
+      int mySize;
+      myRank =  omp_get_thread_num();
+      mySize = size/omp_get_max_threads();
     
-    int cap = mySize + (mySize*myRank);
-    int myStart = (mySize*myRank);
     
-    START_TIMER(genvals);
+      int cap = mySize + (mySize*myRank);
+      int myStart = (mySize*myRank);
+    
+      #pragma omp barrier
+      START_TIMER(genvals);
 
-    #ifdef _OPENMP
-    #pragma omp parallel
-    #endif
-    for (i = myStart; i < cap; i++) {
+      for (int i = myStart; i < cap; i++) {
         vals[i] = rand();
-    }
-
-    STOP_TIMER(genvals);
+      }
+      #pragma omp barrier
+      STOP_TIMER(genvals);
     
-    for (i = 0; i < size; i++) {
+    }
+    for (int i = 0; i < size; i++) {
         #ifdef CATOUT
         printf("%d\n", vals[i]);
         #else
         fprintf(output, "%d\n", vals[i]);
         #endif
     }
+    #endif
     #ifndef CATOUT
     printf("Time taken: %.6f\n", GET_TIMER(genvals));
     fclose(output);
